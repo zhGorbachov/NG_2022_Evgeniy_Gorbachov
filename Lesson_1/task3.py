dfed65fea75fc4dd47bf9659c527e0179e540c5f
Evgeniy Gorbachov <zekagor@gmail.com>
2022-10-19 14:47:12 +0300

    fixed a little mistake

--- Lesson_1/task3.py
+++ Lesson_1/task3.py
@@ -2,14 +2,16 @@ TimeInSeconds = int(input("How many second do you want to convert: "))
 TimeInMinutes = int(TimeInSeconds / 60)
 TimeInHours = int(TimeInMinutes / 60)
 TimeInDays = int(TimeInHours / 24)
-
-if TimeInSeconds >= 60:
-    TimeInSeconds = TimeInSeconds % 60
+if TimeInSeconds >= 0:
+    if TimeInSeconds >= 60:
+        TimeInSeconds = TimeInSeconds % 60
 
     if TimeInMinutes >= 60:
         TimeInMinutes = TimeInMinutes % 60
 
-        if TimeInHours >= 24:
-            TimeInHours = TimeInHours % 24
+    if TimeInHours >= 24:
+        TimeInHours = TimeInHours % 24
 
-print(str(TimeInDays) + ": " + str(TimeInHours) + ": " + str(TimeInMinutes) + ": " + str(TimeInSeconds))
+    print(str(TimeInDays) + ": " + str(TimeInHours) + ": " + str(TimeInMinutes) + ": " + str(TimeInSeconds))
+else:
+    print("Wrong input type")
