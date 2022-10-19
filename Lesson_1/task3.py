e79de9e2f55065151b18cd76fa62315e0cf43eed
Evgeniy Gorbachov <zekagor@gmail.com>
2022-10-19 14:37:25 +0300

    task 3 reworked

--- Lesson_1/task3.py
+++ Lesson_1/task3.py
@@ -1,6 +1,15 @@
-import datetime
+TimeInSeconds = int(input("How many second do you want to convert: "))
+TimeInMinutes = int(TimeInSeconds / 60)
+TimeInHours = int(TimeInMinutes / 60)
+TimeInDays = int(TimeInHours / 24)
 
-TimeInSecond = float(input("How many second do you want to convert: "))
-TimeAsData = datetime.datetime.fromtimestamp(TimeInSecond)
-print("Time in second => " + str(TimeInSecond))
-print("Time as data => " + str(TimeAsData))
+if TimeInSeconds >= 60:
+    TimeInSeconds = TimeInSeconds % 60
+
+    if TimeInMinutes >= 60:
+        TimeInMinutes = TimeInMinutes % 60
+
+        if TimeInHours >= 24:
+            TimeInHours = TimeInHours % 24
+
+print(str(TimeInDays) + ": " + str(TimeInHours) + ": " + str(TimeInMinutes) + ": " + str(TimeInSeconds))
