b8d60a6ae97a39476adcd00cfee20dc8fe3d693d
Evgeniy Gorbachov <zekagor@gmail.com>
2022-10-21 15:53:57 +0300

    Elif replaced on Match Case

--- Lesson_1/task2.py
+++ Lesson_1/task2.py
@@ -2,17 +2,18 @@ FirstNumber = float(input("Select first number: "))
 SecondNumber = float(input("Select second number: "))
 SelectAction = input("Select action from (+, -, /, *, square, square root): ")
 print("========================")
-if SelectAction == "+":
-    print(FirstNumber + SecondNumber)
-elif SelectAction == "-":
-    print(FirstNumber - SecondNumber)
-elif SelectAction == "*":
-    print(FirstNumber * SecondNumber)
-elif SelectAction == "/":
-    print(FirstNumber / SecondNumber)
-elif SelectAction == "square root":
-    print("FirstNumber = " + str(FirstNumber**0.5) + " SecondNumber = " + str(SecondNumber**0.5))
-elif SelectAction == "square":
-    print("FirstNumber = " + str(FirstNumber**2) + " SecondNumber = " + str(SecondNumber**2))
-else:
-    print("Wrong action!")
+match SelectAction:
+    case '+':
+        print(FirstNumber + SecondNumber)
+    case '-':
+        print(FirstNumber - SecondNumber)
+    case '*':
+        print(FirstNumber * SecondNumber)
+    case '/':
+        print(FirstNumber / SecondNumber)
+    case 'square':
+        print("FirstNumber = " + str(FirstNumber ** 2) + " SecondNumber = " + str(SecondNumber ** 2))
+    case 'square root':
+        print("FirstNumber = " + str(FirstNumber**0.5) + " SecondNumber = " + str(SecondNumber**0.5))
+    case other:
+        print("Wrong action!")
