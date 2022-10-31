759ae9a901003f2cc45c082338895d424c1c2668
Evgeniy Gorbachov <zekagor@gmail.com>
2022-10-31 15:40:47 +0200

    Task updated, now sum calculates without List[0][-1].

--- Lesson_2/task5.py
+++ Lesson_2/task5.py
@@ -1,10 +1,11 @@
 Numbers = input("Enter random int numbers: ")
 List = Numbers.split(", ", -1)
-
 for elements in range(len(List)):
     List[elements] = int(List[elements])
 
 List.sort()
 print("Min values = " + str(List[0]) + ";" + " Max values = " + str(List[-1]))
-SumOfCentralNumbers = sum(List)-int(List[0])-int(List[-1])
-print("Sum of central numbers = " + str(SumOfCentralNumbers))
+MinNumber, MaxNumber = List[0], List[-1]
+List.remove(MinNumber)
+List.remove(MaxNumber)
+print("Sum of central numbers = " + str(sum(List)))
