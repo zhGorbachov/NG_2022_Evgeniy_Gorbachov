283371e6a0ce1991c3bab080ef45985aac8901ef
Evgeniy Gorbachov <zekagor@gmail.com>
2022-11-15 21:07:26 +0200

    In the task was update menu and txt. file, and add more function.

--- Lesson_4/task1.py
+++ Lesson_4/task1.py
@@ -2,7 +2,7 @@ from rich.console import Console
 import platform
 import inquirer
 from inquirer.themes import GreenPassion
-
+import psutil
 
 console = Console()
 style = "black on #e0cfb1"
@@ -14,10 +14,16 @@ def createfile():
     return datafile
 
 
-def appendfile(message, function):
+def appendfile(number, function, key):
     datafile = open("InfoAboutPC.txt", "a")
-    datafile.write(message + str(function) + "\n")
+    datafile.write("======================[" + str(key) + "]======================" + "\n")
+    datafile.write("[" + str(number) + "] " + str(function) + "\n")
+    datafile.write("\n")
     datafile.close()
+
+
+def find_virtualMemory():
+    return psutil.virtual_memory()
 
 
 def findProcessor():
@@ -32,6 +38,14 @@ def findArchitecture():
     return platform.architecture()
 
 
+def findPhysicalCoresOfCPU():
+    return psutil.cpu_count()
+
+
+def findLogicalCoresOfCPU():
+    return psutil.cpu_count(logical=False)
+
+
 def findNode():
     return platform.node()
 
@@ -44,9 +58,8 @@ def findVersionOfPython():
     return platform.python_version()
 
 
-def info():
-    console.print("Program started", style=style, justify="left")
-    console.print("[INFO] Choose variant on space, and confirm on enter.", style='black on #e0cfb1', justify='left')
+def info(message):
+    return console.rule(message, style='#e0cfb1')
 
 
 def menu():
@@ -54,11 +67,14 @@ def menu():
             message="What are you interested in?",
             choices=[
                 ("CPU", "c"),
+                ("Logical cores", 'lc'),
+                ("Physical cores", 'pc'),
                 ("Architecture", "a"),
                 ("Machine", "m"),
                 ("Node name", "n"),
                 ("System", "s"),
-                ("Python version", "pv")], ), ]
+                ("Python version", "pv"),
+                ("Virtual memory", "vm")], ), ]
 
     answer_menu = inquirer.prompt(action_menu, theme=GreenPassion())
     result_menu = []
@@ -68,31 +84,43 @@ def menu():
 
 
 def process():
-    info()
+    info("Program stated")
+    info("[INFO] Choose variant on space, and confirm on enter.")
     createfile()
     result_menu = menu()
+    number_of_count = 1
     while True:
         if 'c' in result_menu:
-            appendfile("Name of CPU: ", findProcessor())
+            appendfile(number_of_count, findProcessor(), "Name of CPU")
             result_menu.remove("c")
+        elif 'pc' in result_menu:
+            appendfile(number_of_count, findPhysicalCoresOfCPU(), "Physical cores")
+            result_menu.remove("pc")
+        elif 'lc' in result_menu:
+            appendfile(number_of_count, findLogicalCoresOfCPU(), "Logical cores")
+            result_menu.remove("lc")
         elif 'a' in result_menu:
-            appendfile("Architecture of PC: ", findArchitecture())
+            appendfile(number_of_count, findArchitecture(), "Architecture")
             result_menu.remove('a')
         elif 'm' in result_menu:
-            appendfile("Name of machine: ", findMachine())
+            appendfile(number_of_count, findMachine(), "Name of machine")
             result_menu.remove('m')
         elif 'n' in result_menu:
-            appendfile("Node name: ", findNode())
+            appendfile(number_of_count, findNode(), "Node name")
             result_menu.remove('n')
         elif 's' in result_menu:
-            appendfile("Name of system: ", findSystem())
+            appendfile(number_of_count, findSystem(), "Name of system")
             result_menu.remove('s')
         elif 'pv' in result_menu:
-            appendfile("Version of python: ", findVersionOfPython())
+            appendfile(number_of_count, findVersionOfPython(), "Python version")
             result_menu.remove('pv')
+        elif 'vm' in result_menu:
+            appendfile(number_of_count, find_virtualMemory(), "Virtual memory")
+            result_menu.remove('vm')
         else:
             break
-    console.print("Program shuts down.", style="black on #e0cfb1", justify='left')
+        number_of_count += 1
+    info("Program shuts down.")
 
 
 def main():
